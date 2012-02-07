e7abc5b46b29c20cf3438c7cb7099cce14e6f13d
paalgyula <paalgyula@localhost>
2012-02-07 12:44:41 +0100

    File separator alapjan el lett dontve, milyen LAF kerul alkalmazasra, border leveve a loading screenrol, es dialogga alakitva.

--- src/main/java/com/gw_systems/invoicer/frames/MainFrame.java
+++ src/main/java/com/gw_systems/invoicer/frames/MainFrame.java
@@ -1,9 +1,11 @@
 package com.gw_systems.invoicer.frames;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.ImageIcon;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,10 +19,11 @@ import com.gw_systems.invoicer.DatabaseConnector;
 import com.gw_systems.invoicer.ImagePanel;
 import java.awt.Color;
 import java.awt.Toolkit;
+import java.io.File;
 
 public class MainFrame {
 
-	private JFrame frmGwinvoicerHomeEdition;
+	private JDialog frmGwinvoicerHomeEdition;
 	private JLabel lblStatusindicator;
 	private JProgressBar progressBar;
 
@@ -34,7 +37,12 @@ public class MainFrame {
 			public void run() {
 				try {
 					JFrame.setDefaultLookAndFeelDecorated( true );
-					UIManager.setLookAndFeel( "com.sun.java.swing.plaf.windows.WindowsLookAndFeel" );
+					
+					if ( String.valueOf( File.separatorChar ).equals( "/" ) )
+						UIManager.setLookAndFeel( "com.sun.java.swing.plaf.gtk.GTKLookAndFeel" );
+					else
+						UIManager.setLookAndFeel( "com.sun.java.swing.plaf.windows.WindowsLookAndFeel" );
+					
 					
 					MainFrame window = new MainFrame();
 					window.frmGwinvoicerHomeEdition.setVisible(true);
@@ -58,21 +66,36 @@ public class MainFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmGwinvoicerHomeEdition = new JFrame();
+		frmGwinvoicerHomeEdition = new JDialog();
+		frmGwinvoicerHomeEdition.setResizable(false);
+		
+		BorderLayout borderLayout = (BorderLayout) frmGwinvoicerHomeEdition.getContentPane().getLayout();
+		borderLayout.setVgap(3);
+		borderLayout.setHgap(3);
 		frmGwinvoicerHomeEdition.setIconImage(Toolkit.getDefaultToolkit().getImage(MainFrame.class.getResource("/icons/invoice_bigicon.png")));
 		frmGwinvoicerHomeEdition.setBackground(Color.WHITE);
 		frmGwinvoicerHomeEdition.setTitle("GW-Invoicer Home Edition");
-		frmGwinvoicerHomeEdition.setBounds(100, 100, 462, 317);
-		frmGwinvoicerHomeEdition.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmGwinvoicerHomeEdition.setBounds(100, 100, 462, 297);
+		frmGwinvoicerHomeEdition.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		frmGwinvoicerHomeEdition.setUndecorated(true);
+		frmGwinvoicerHomeEdition.setBackground(Color.WHITE);
+		
+		Toolkit tk = Toolkit.getDefaultToolkit();
+	    Dimension screenSize = tk.getScreenSize();
+	    int screenHeight = screenSize.height;
+	    int screenWidth = screenSize.width;
+	    int windowHeight = frmGwinvoicerHomeEdition.getHeight();
+	    int windowWidth = frmGwinvoicerHomeEdition.getWidth();
+	    frmGwinvoicerHomeEdition.setLocation(screenWidth / 2 - windowWidth / 2, screenHeight / 2 - windowHeight / 2);
 		
 		progressBar = new JProgressBar();
 		frmGwinvoicerHomeEdition.getContentPane().add(progressBar, BorderLayout.SOUTH);
 		
 		ImagePanel panel = new ImagePanel( new ImageIcon( getClass().getResource("/icons/bg.jpg") ).getImage() );
 		frmGwinvoicerHomeEdition.getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(new BorderLayout(0, 0));
+		panel.setLayout(new BorderLayout(3, 3));
 		
-		lblStatusindicator = new JLabel(" StatusIndicator");
+		lblStatusindicator = new JLabel("");
 		panel.add(lblStatusindicator, BorderLayout.SOUTH);
 	}
 
